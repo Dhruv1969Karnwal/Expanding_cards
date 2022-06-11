c2c7700964a575e0374e4a4d072796acb8777591
Dhruv karnwal <104852803+Dhruv1969Karnwal@users.noreply.github.com>
2022-06-11 18:56:57 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,143 @@
+const image_2 = document.querySelector("#image_2");
+const image_3 = document.querySelector("#image_3");
+const image_1 = document.querySelector("#image_1");
+const image_4 = document.querySelector("#image_4");
+const image_5 = document.querySelector("#image_5");
+const text_1 = document.querySelector("#text_1");
+const text_2 = document.querySelector("#text_2");
+const text_3 = document.querySelector("#text_3");
+const text_4 = document.querySelector("#text_4");
+const text_5 = document.querySelector("#text_5");
+
+
+function display_1(){
+    image_1.style.width = "65rem";
+  image_1.style.transition = ".75s ease-in-out";
+  text_1.style.opacity = '1';
+  text_1.style.transition = "all 1.5s ease-in-out";
+  if(image_2.style.width>"15rem"){
+    image_2.style.width = "15rem"; 
+    text_2.style.opacity = '0';
+    text_2.style.transition = "all .1s ease-in-out";
+  }
+  if(image_3.style.width>"15rem"){
+    image_3.style.width = "15rem";
+    text_3.style.opacity = '0';
+    text_3.style.transition = "all .1s ease-in-out";
+  }
+  if(image_4.style.width>"15rem"){
+    image_4.style.width = "15rem";
+    text_4.style.opacity = '0';
+    text_4.style.transition = "all .1s ease-in-out";
+  }
+  if(image_5.style.width>"15rem"){
+    image_5.style.width = "15rem";
+    text_5.style.opacity = '0';
+    text_5.style.transition = "all .1s ease-in-out";
+  }
+}
+function display_2(){
+  image_2.style.width = "65rem";
+  image_2.style.transition = ".75s ease-in-out";
+  text_2.style.opacity = '1';
+  text_2.style.transition = "all 1.5s ease-in-out";
+  if(image_1.style.width>"15rem"){
+    image_1.style.width = "15rem";
+    text_1.style.opacity = "0";
+    text_1.style.transition = "all .1s ease-in-out";
+  }
+  if(image_3.style.width>"15rem"){
+    image_3.style.width = "15rem";
+    text_3.style.opacity = "0";
+    text_3.style.transition = "all .1s ease-in-out";
+  }
+  if(image_4.style.width>"15rem"){
+    image_4.style.width = "15rem";
+    text_4.style.opacity = "0";
+    text_4.style.transition = "all .1s ease-in-out";
+  }
+  if(image_5.style.width>"15rem"){
+    image_5.style.width = "15rem";
+    text_5.style.opacity = "0";
+    text_5.style.transition = "all .1s ease-in-out";
+  }
+}
+function display_3(){
+  image_3.style.width = "65rem";
+  image_3.style.transition = ".75s ease-in-out";
+  text_3.style.opacity = '1';
+  text_3.style.transition = "all 1.5s ease-in-out";
+  if(image_1.style.width>"15rem"){
+    image_1.style.width = "15rem";
+    text_1.style.opacity = "0";
+    text_1.style.transition = "all .1s ease-in-out";
+  }
+  if(image_2.style.width>"15rem"){
+    image_2.style.width = "15rem";
+    text_2.style.opacity = "0";
+    text_2.style.transition = "all .1s ease-in-out";
+  }
+  if(image_4.style.width>"15rem"){
+    image_4.style.width = "15rem";
+    text_4.style.opacity = "0";
+    text_4.style.transition = "all .1s ease-in-out";
+  }
+  if(image_5.style.width>"15rem"){
+    image_5.style.width = "15rem";
+    text_5.style.opacity = "0";
+    text_5.style.transition = "all .1s ease-in-out";
+  }
+}
+function display_4(){
+  image_4.style.width = "65rem";
+  image_4.style.transition = ".75s ease-in-out";
+  text_4.style.opacity = '1';
+  text_4.style.transition = "all 1.5s ease-in-out";
+  if(image_1.style.width>"15rem"){
+    image_1.style.width = "15rem";
+    text_1.style.opacity = "0";
+    text_1.style.transition = "all .1s ease-in-out";
+  }
+  if(image_3.style.width>"15rem"){
+    image_3.style.width = "15rem";
+    text_3.style.opacity = "0";
+    text_3.style.transition = "all .1s ease-in-out";
+  }
+  if(image_2.style.width>"15rem"){
+    image_2.style.width = "15rem";
+    text_2.style.opacity = "0";
+    text_2.style.transition = "all .1s ease-in-out";
+  }
+  if(image_5.style.width>"15rem"){
+    image_5.style.width = "15rem";
+    text_5.style.opacity = "0";
+    text_5.style.transition = "all .1s ease-in-out";
+  }
+}
+function display_5(){
+  image_5.style.width = "65rem";
+  image_5.style.transition = ".75s ease-in-out";
+  text_5.style.opacity = '1';
+  text_5.style.transition = "all 1.5s ease-in-out";
+  if(image_1.style.width>"15rem"){
+    image_1.style.width = "15rem";
+    text_1.style.opacity = "0";
+    text_1.style.transition = "all .1s ease-in-out";
+  }
+  if(image_3.style.width>"15rem"){
+    image_3.style.width = "15rem";
+    text_3.style.opacity = "0";
+    text_3.style.transition = "all .1s ease-in-out";
+  }
+  if(image_4.style.width>"15rem"){
+    image_4.style.width = "15rem";
+    text_4.style.opacity = "0";
+    text_4.style.transition = "all .1s ease-in-out";
+  }
+  if(image_2.style.width>"15rem"){
+    image_2.style.width = "15rem";
+    text_2.style.opacity = "0";
+    text_2.style.transition = "all .1s ease-in-out";
+  }
+}
+
